32ba1ffc5b06b2fd6bad79a1eace0131ef07a880
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2018-10-02 10:41:26 +0200

    “” “”

--- sites/sync_sitename.sh
+++ sites/sync_sitename.sh
@@ -4,11 +4,11 @@
 # Set your site specific variables
 # ------------------------------------------------------------------------------
 
-# Production enviroment
-export PRODWWW=sitedir
+# Production enviroment (Name of directory)
+export PRODWWW=web_site_directory
 
-# Beta enviroment
-export BETAWWW=sitedir_beta
+# Beta enviroment (Name of directory)
+export BETAWWW=web_site_directory_beta
 
 # Production domain
 export PRODURL=domain.se
@@ -16,9 +16,9 @@ export PRODURL=domain.se
 # Beta domain
 export BETAURL=beta.domain.se
 
-# Path to production vhost file
+# Path to production vhost file (filename)
 export PRODVHOST=vhostfile
 
-# Path to beta vhost file
+# Path to beta vhost file (filename)
 export BETAVHOST=vhostfile_beta
 
