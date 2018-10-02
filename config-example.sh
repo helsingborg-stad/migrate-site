ceb939646a064d26c61441624809f64cb39887dd
Carl Johan Silvergrund <johan.silvergrund@hiq.se>
2018-10-02 14:00:11 +0200

    Renaming files to make it more understandeble

---
+++ config-example.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------------
+# Set your server specific variables
+# ------------------------------------------------------------------------------
+
+# Absolute path to vhost directory
+# Example: /etc/apache2/conf.d/
+export VHOSTDIR=/path_to_vhost
+
+# Absolute path to www folder where you have all your website directories
+# Example: /var/www/
+export SITEDIR=/path_to_www
+
+# Direcory name of this app (migrate-site)
+# Change to what ever you want this folder to be called
+export SYNCSCRIPT=migrate-site
+
+
---
+++ sites/sitename.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# ------------------------------------------------------------------------------
+# Set your site specific variables
+# ------------------------------------------------------------------------------
+
+# Production enviroment (Name of directory)
+export PRODWWW=web_site_directory
+
+# Beta enviroment (Name of directory)
+export BETAWWW=web_site_directory_beta
+
+# Production domain
+export PRODURL=domain.se
+
+# Beta domain
+export BETAURL=beta.domain.se
+
+# Path to production vhost file (filename)
+export PRODVHOST=vhostfile
+
+# Path to beta vhost file (filename)
+export BETAVHOST=vhostfile_beta
+
